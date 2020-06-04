780f9c15e18ff8e0c6daf814353ea5c57ecc9b02
Rachel Isaac-Wilson <rachel.isaac.wilson@gmail.com>
2020-06-04 11:56:31 +0100

    2nd

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
-function bmi(weight, height) {
-      
+function calculateBmi() {
+let weight = document.bmiCal.weight.value;
+let height = document.bmiCal.height.value;
+let bmi = weight/(height/100*height/100);
 
-  let bmi = weight / (height**2);
-  
-  
-    if (bmi < 15){
-    return "Error";
-  } else if(bmi > 15 && bmi < 18.5){
-    return "Underweight";
-  }else if (bmi >= 18.5 && bmi <25){
-    return "Healthy weight";
-  }else if (bmi >= 25 && bmi <30){
-    return "Overweight";
-  }else if (bmi >=30 && bmi <45){
-    return "Obese";
-  }else {
-    return "Error"
-  } 
+document.bmiCal.bmi.value = bmi;
+
+if(bmi >= 15 && bmi < 18.5){
+document.bmiCal.meaning.value = "Underweight";
+}
+else if(bmi >= 18.5 && bmi < 25){
+document.bmiCal.meaning.value = "Healthy Weight";
+}
+else if(bmi >= 25 && bmi <30){
+document.bmiCal.meaning.value = "Overweight"
+}
+else if(bmi >=30 && bmi <45){    
+document.bmiCal.meaning.value = "Obese";  
+}else {   
+document.bmiCal.meaning.value = "Error"  } 
 }
 
 
-function ClearForm(form){
-
-  form.weight.value = "";
-  form.height.value = "";
-  form.bmi.value = "";
-}
-    
-    
-
-  	
